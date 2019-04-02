baa114606d5a35f6aabd179c3ce375f361417411
Shyrick <lykun@mail.ru>
2019-04-02 23:27:43 +0300

    From Home

--- CoreByMySelfe/src/Lesson3/L3_p2_IfElseExample.java
+++ CoreByMySelfe/src/Lesson3/L3_p2_IfElseExample.java
@@ -1,8 +1,18 @@
 package Lesson3;
 
+import java.util.Scanner;
+
 public class L3_p2_IfElseExample {
     public static void main(String[] args) {
         // time 16:20
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+
+        if (x % 2 == 0){
+            System.out.println("Число " + x + " кратно 2");
+        }else {
+            System.out.println("Число " + x + " не кратно 2");
+        }
     }
 
 }
