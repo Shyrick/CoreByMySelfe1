ec4d3959549e8acf1f5b3b4d9b25233d37a01f2f
technical.team <Lykun@mail.ru>
2019-04-12 20:48:14 +0300

    From work lection 3

--- CoreByMySelfe/src/Lesson3/Arrays/L3_p9_BubbleSortExample.java
+++ CoreByMySelfe/src/Lesson3/Arrays/L3_p9_BubbleSortExample.java
@@ -1,9 +1,39 @@
 package Lesson3.Arrays;
 
+import java.util.Scanner;
+
 public class L3_p9_BubbleSortExample {
 
     public static void main(String[] args) {
 
         // Time 01:54:00
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("Введите длину массива");
+        int length = scanner.nextInt();
+
+        System.out.println("Введите массив интов в одну строку через пробел");
+        int[] array = new int[length];
+
+
+        for (int i = 0; i < array.length ; i++) {
+            array [i] = scanner.nextInt();
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < (array.length - i); j++) {
+                if (array[j - 1] > array[j]) {
+                    //swap elements
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+
+            }
+        }
+            for (int value:array) {
+                System.out.print(value + " ");
+            }
+
     }
 }
