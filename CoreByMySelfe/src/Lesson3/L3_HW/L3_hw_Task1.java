5bf1a1a3d6cd46971fff29cee97800b9165385ef
technical.team <Lykun@mail.ru>
2019-04-13 20:32:14 +0300

    From work lection 3

---
+++ CoreByMySelfe/src/Lesson3/L3_HW/L3_hw_Task1.java
@@ -0,0 +1,8 @@
+package Lesson3.L3_HW;
+
+public class L3_hw_Task1 {
+
+    public static void main(String[] args) {
+
+    }
+}
--- CoreByMySelfe/src/Lesson3/L3_Practice/L3_pr_Task6.java
+++ CoreByMySelfe/src/Lesson3/L3_Practice/L3_pr_Task6.java
@@ -1,4 +1,34 @@
 package Lesson3.L3_Practice;
 
+import java.util.Scanner;
+
 public class L3_pr_Task6 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("введите ширину и высоту прямоугольника");
+        int whide = scanner.nextInt();
+        int hight = scanner.nextInt();
+//  i=0 - х
+//  i=n - x
+//  j=0 - x
+//  j=n - x
+        int number = 100;
+        for (int i = 0; i < hight; i++) {
+            for (int j = 0; j < whide; j++) {
+
+                if (i == 0 || i == (hight - 1) || j == 0 || j == (whide - 1) ){
+                    System.out.print("xxx ");
+                }else {
+                    System.out.print(number + " ");
+                    number++;
+                }
+            }
+            System.out.println();
+        }
+
+
+
+    }
 }
