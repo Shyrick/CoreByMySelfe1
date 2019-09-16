940e4ad87a490589194993cc27e7405bf2718ae6
Shyrick <lykun@mail.ru>
2019-09-16 22:23:33 +0300

    From Home lection 4_p3

--- CoreByMySelfe/src/Lesson4/L4_p3_RecursionExample.java
+++ CoreByMySelfe/src/Lesson4/L4_p3_RecursionExample.java
@@ -1,14 +1,25 @@
 package Lesson4;
 
-public class L4_p3_RecursionExample { STOP 1:03:00
+public class L4_p3_RecursionExample {
 
     static String printNubers (int n){
         if (n <= 0 ) return "";
+        String str = printNubers(n-1);
+        return  str + " " + n;
+    }
 
-        return printNubers(n-1) + " " + n;
+    static int factorial (int n){
+        if(n <= 0) return 1;
+        int value = factorial(n-1);
+        return value * n;
     }
     public static void main(String[] args) {
         String str = printNubers(3);
         System.out.println(str);
+
+        for (int i = 0; i < 15; i++) {
+            System.out.println("Factorial (" + i + ")" + factorial(i));
+        }
+
     }
 }
