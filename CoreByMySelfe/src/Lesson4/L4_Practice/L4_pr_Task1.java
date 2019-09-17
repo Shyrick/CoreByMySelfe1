69aaf10f9baa24900b31892125c233b7c444a0a4
Shyrick <lykun@mail.ru>
2019-09-18 00:38:42 +0300

    From Home practice 4_pr_1

---
+++ CoreByMySelfe/src/Lesson4/L4_Practice/L4_pr_Task1.java
@@ -0,0 +1,24 @@
+package Lesson4.L4_Practice;
+
+public class L4_pr_Task1 {
+
+    static int maxIndex (int [] arr){
+        int max = arr [0];
+        int maxIndex = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr [i] > max) {
+                max = arr [i];
+                maxIndex = i;
+            }
+        } return maxIndex;
+    }
+    static int maxInArray (int [] arr){
+        return arr [maxIndex(arr)];
+    }
+
+    public static void main(String[] args) {
+        int[] array =  {1,7,105, 5, 6};
+        System.out.println("Index of max = " + maxIndex(array));
+        System.out.println("Max in array = " + maxInArray(array));
+    }
+}
