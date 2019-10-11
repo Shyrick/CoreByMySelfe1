1ec3445a408bf67b0067e5e7c4e4c2819aac3b84
technical.team <Lykun@mail.ru>
2019-10-11 20:43:43 +0300

    From work 4_hw_5

--- CoreByMySelfe/src/Lesson4/L4_HW/L4_hw_Task3.java
+++ CoreByMySelfe/src/Lesson4/L4_HW/L4_hw_Task3.java
@@ -5,6 +5,19 @@ public class L4_hw_Task3 {
 //    Написать функцию getMax которая принимает на вход два аргумента в виде чисел.
 //    А возвращает максимальное из этих двух.
 //    Также, необходимо перегрузить эту функцию для работы с разными числовыми типами переменных (int, float)
+    static int getMax(int a, int b){
+        int max = 0;
+        if (a > b) return max = a;
+    return max = b;
+    }
+    static float getMax(float a, float b){
+        float max = 0;
+        if (a > b) return max = a;
+        return max = b;
+    }
+    public static void main(String[] args) {
+        System.out.println(getMax(100, 6));
+        System.out.println(getMax(8.6f, 7.0f));
 
-    
+    }
 }
---
+++ CoreByMySelfe/src/Lesson4/L4_HW/L4_hw_Task5.java
@@ -0,0 +1,10 @@
+package Lesson4.L4_HW;
+
+public class L4_hw_Task5 {
+//    Решить задачу 2, без использования циклов. Используя рекурсию.
+
+
+    public static void main(String[] args) {
+        
+    }
+}
