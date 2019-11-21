c82869fb2db430039ca5ea73b598b2d8f3d1220c
technical.team <Lykun@mail.ru>
2019-11-21 20:41:37 +0300

    From work L51 HW

--- CoreByMySelfe/src/Lesson5_1/L51_HW/SnowMan.java
+++ CoreByMySelfe/src/Lesson5_1/L51_HW/SnowMan.java
@@ -5,6 +5,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -29,25 +30,15 @@ public class SnowMan {
 
         double radius = random();
         double x = primaryStage.getWidth()/2;
-        double y = primaryStage.getHeight() - radius;
-        System.out.println("rad " + radius);
-        System.out.println("MH " + primaryStage.getHeight());
-        System.out.println("MW " + primaryStage.getWidth());
-        System.out.println("x " + x + " y " + y);
-//        Circle circle = new Circle(x, y, radius);
-        Circle circle1 = new Circle(300, 600, 2);
-        Circle circle2 = new Circle(300, 550, 2);
-        Circle circle3 = new Circle(300, 500, 2);
-        Circle circle4 = new Circle(300, 450, 2);
-        Circle circle5 = new Circle(300, 400, 2);
-        Circle circle6 = new Circle(550, 20, 2);
-        Circle circle7 = new Circle(550, 40, 2);
-        Circle circle8 = new Circle(550, 60, 2);
-        Circle circle9 = new Circle(550, 80, 2);
-        Circle circle10 = new Circle(550, 0, 2);
-//        circle.setStrokeWidth(10);
-//        circle.setFill(Paint.valueOf("#00000000"));
-        frontRoot.getChildren().addAll( circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8, circle9, circle10);
+        double y = primaryStage.getHeight() - 50 - radius;
+        double strokeWhide = 2;
+        Line line = new Line(50, (primaryStage.getHeight() - 50), 600, (primaryStage.getHeight() - 50) );
+
+        Circle circle = new Circle(x, y, radius);
+        circle.setStrokeWidth(strokeWhide);
+        circle.setFill(Paint.valueOf("#FFFFFF"));
+        circle.setStroke(Paint.valueOf("#000000"));
+        frontRoot.getChildren().addAll( circle, line);
     }
 
     public void textAndButton (Pane root, Pane frontRoot, Stage primaryStage ){
